cfe976713732327a582f02bcd7f4553314216262
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2022-02-18 00:29:07 +0300

    kütüphaneler

---
+++ fal_application_2/Pods/Target Support Files/Pods-fal_application_2/Pods-fal_application_2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fal_application_2 : NSObject
+@end
+@implementation PodsDummy_Pods_fal_application_2
+@end
---
+++ fal_application_2/fal_application_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fal_application_2
+//
+//  Created by Selin Çağlar on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
